ede186c658a9786be29d3f4efa6dfa38b7e7f36a
BrenoOliveiradev <brenooliveira.dev@hotmail.com>
2025-05-26 23:49:03 -0300

    add const em valores e valores a converter e innerHTML

--- scripts.js
+++ scripts.js
@@ -2,10 +2,14 @@ const convertButton = document.querySelector(".convert-button")
 
 function convertValues(){
     const inputCurrencyValue = document.querySelector(".input-currency").value
-
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //Valor em Real
+    const currencyValueConverted = document.querySelector(".currency-value") //Outras Moedas
     const dolarToday = 5.2
 
     const convertedValue = inputCurrencyValue / dolarToday
+
+    currencyValueToConvert.innerHTML = inputCurrencyValue
+    currencyValueConverted.innerHTML = convertedValue
 
     console.log(convertedValue)
 }
